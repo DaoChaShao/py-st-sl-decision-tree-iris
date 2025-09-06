5985d62d3329ac2661a35a4361651562ce65b4c9
DaoChaShao <27882272@qq.com>
2025-09-06 15:24:01 +0800

    feat: add preparation.py for initial setup and configuration

---
+++ subpages/preparation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/9/6 15:07
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   preparation.py
+# @Desc     :   
+
